3d88498aa38d2d0a1f0193c6e4cce263ebcb68c7
Denis Enes <55022112+denisenes@users.noreply.github.com>
2020-09-11 13:23:07 +0700

    Add files via upload

---
+++ HelloWorld/src/main/java/HelloWorld.java
@@ -0,0 +1,10 @@
+public class HelloWorld {
+
+    public static void main(String args[]) {
+        System.out.println("HELLO HAHAHAHA");
+        System.out.println("Second string");
+        int myAge = 10;
+        System.out.println(myAge + " years");
+    }
+
+}
